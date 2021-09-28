c5b124a271bf9d580a748fc658211b2d675a5035
AlanVenic <91500958+AlanVenic@users.noreply.github.com>
2021-09-28 18:38:53 -0300

    Add files via upload

---
+++ Animated_Background/animatedBG.py
@@ -0,0 +1,227 @@
+import pygame, sys, os
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init()
+pygame.mixer.init()
+spriteGroup = pygame.sprite.OrderedUpdates()
+textboxGroup = pygame.sprite.OrderedUpdates()
+gameClock = pygame.time.Clock()
+musicPaused = False
+hiddenSprites = pygame.sprite.OrderedUpdates()
+screenRefresh = True
+background = None
+
+
+def parseColour(colour):
+    if type(colour) == str:
+        # check to see if valid colour
+        return pygame.Color(colour)
+    else:
+        colourRGB = pygame.Color("white")
+        colourRGB.r = colour[0]
+        colourRGB.g = colour[1]
+        colourRGB.b = colour[2]
+        return colourRGB
+
+
+def loadImage(fileName, useColorKey=False):
+    if os.path.isfile(fileName):
+        image = pygame.image.load(fileName)
+        image = image.convert_alpha()
+        # Return the image
+        return image
+    else:
+        raise Exception(f"Error loading image: {fileName} – Check filename and path?")
+
+class Background():
+    def __init__(self):
+        self.colour = pygame.Color("black")
+
+    def setTiles(self, tiles):
+        if type(tiles) is str:
+            self.tiles = [[loadImage(tiles)]]
+        elif type(tiles[0]) is str:
+            self.tiles = [[loadImage(i) for i in tiles]]
+        else:
+            self.tiles = [[loadImage(i) for i in row] for row in tiles]
+        self.stagePosX = 0
+        self.stagePosY = 0
+        self.tileWidth = self.tiles[0][0].get_width()
+        self.tileHeight = self.tiles[0][0].get_height()
+        screen.blit(self.tiles[0][0], [0, 0])
+        self.surface = screen.copy()
+
+    def scroll(self, x, y):
+        self.stagePosX -= x
+        self.stagePosY -= y
+        col = (self.stagePosX % (self.tileWidth * len(self.tiles[0]))) // self.tileWidth
+        xOff = (0 - self.stagePosX % self.tileWidth)
+        row = (self.stagePosY % (self.tileHeight * len(self.tiles))) // self.tileHeight
+        yOff = (0 - self.stagePosY % self.tileHeight)
+
+        col2 = ((self.stagePosX + self.tileWidth) % (self.tileWidth * len(self.tiles[0]))) // self.tileWidth
+        row2 = ((self.stagePosY + self.tileHeight) % (self.tileHeight * len(self.tiles))) // self.tileHeight
+        screen.blit(self.tiles[row][col], [xOff, yOff])
+        screen.blit(self.tiles[row][col2], [xOff + self.tileWidth, yOff])
+        screen.blit(self.tiles[row2][col], [xOff, yOff + self.tileHeight])
+        screen.blit(self.tiles[row2][col2], [xOff + self.tileWidth, yOff + self.tileHeight])
+
+        self.surface = screen.copy()
+
+    def setColour(self, colour):
+        self.colour = parseColour(colour)
+        screen.fill(self.colour)
+        pygame.display.update()
+        self.surface = screen.copy()
+
+def updateDisplay():
+    global background
+    spriteRects = spriteGroup.draw(screen)
+    textboxRects = textboxGroup.draw(screen)
+    pygame.display.update()
+    keys = pygame.key.get_pressed()
+    if (keys[pygame.K_ESCAPE]):
+        pygame.quit()
+        sys.exit()
+    spriteGroup.clear(screen, background.surface)
+    textboxGroup.clear(screen, background.surface)
+
+class newSprite(pygame.sprite.Sprite):
+    def __init__(self, filename, frames=1, altDims = None):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        img = loadImage(filename)
+        if altDims:
+            img = pygame.transform.scale(img, (altDims[0]*frames, altDims[1]))
+        self.originalWidth = img.get_width() // frames
+        self.originalHeight = img.get_height()
+        frameSurf = pygame.Surface((self.originalWidth, self.originalHeight), pygame.SRCALPHA, 32)
+        x = 0
+        for frameNo in range(frames):
+            frameSurf = pygame.Surface((self.originalWidth, self.originalHeight), pygame.SRCALPHA, 32)
+            frameSurf.blit(img, (x, 0))
+            self.images.append(frameSurf.copy())
+            x -= self.originalWidth
+        self.image = pygame.Surface.copy(self.images[0])
+
+        self.currentImage = 0
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (0, 0)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.angle = 0
+        self.scale = 1
+
+    def addImage(self, filename):
+        self.images.append(loadImage(filename))
+
+    def move(self, xpos, ypos, centre=False):
+        if centre:
+            self.rect.center = [xpos, ypos]
+        else:
+            self.rect.topleft = [xpos, ypos]
+
+    def changeImage(self, index):
+        self.currentImage = index
+        if self.angle == 0 and self.scale == 1:
+            self.image = self.images[index]
+        else:
+            self.image = pygame.transform.rotozoom(self.images[self.currentImage], -self.angle, self.scale)
+        oldcenter = self.rect.center
+        self.rect = self.image.get_rect()
+        originalRect = self.images[self.currentImage].get_rect()
+        self.originalWidth = originalRect.width
+        self.originalHeight = originalRect.height
+        self.rect.center = oldcenter
+        self.mask = pygame.mask.from_surface(self.image)
+        if screenRefresh:
+            updateDisplay()
+
+def moveSprite(sprite, x, y, centre=False):
+    sprite.move(x, y, centre)
+    if screenRefresh:
+        updateDisplay()
+
+
+def transformSprite(sprite, angle, scale, hflip=False, vflip=False):
+    oldmiddle = sprite.rect.center
+    if hflip or vflip:
+        tempImage = pygame.transform.flip(sprite.images[sprite.currentImage], hflip, vflip)
+    else:
+        tempImage = sprite.images[sprite.currentImage]
+    if angle != 0 or scale != 1:
+        sprite.angle = angle
+        sprite.scale = scale
+        tempImage = pygame.transform.rotozoom(tempImage, -angle, scale)
+    sprite.image = tempImage
+    sprite.rect = sprite.image.get_rect()
+    sprite.rect.center = oldmiddle
+    sprite.mask = pygame.mask.from_surface(sprite.image)
+    if screenRefresh:
+        updateDisplay()
+
+
+def showSprite(sprite):
+    spriteGroup.add(sprite)
+    if screenRefresh:
+        updateDisplay()
+
+def makeSprite(filename, frames=1, altDims = None):
+    thisSprite = newSprite(filename, frames, altDims)
+    return thisSprite
+
+def changeSpriteImage(sprite, index):
+    sprite.changeImage(index)
+
+def clock():
+    current_time = pygame.time.get_ticks()
+    return current_time
+
+def screenSize(sizex, sizey, xpos=None, ypos=None, fullscreen=False):
+    global screen
+    global background
+    if xpos != None and ypos != None:
+        os.environ['SDL_VIDEO_WINDOW_POS'] = f"{xpos}, {ypos + 50}"
+    else:
+        windowInfo = pygame.display.Info()
+        monitorWidth = windowInfo.current_w
+        monitorHeight = windowInfo.current_h
+        os.environ['SDL_VIDEO_WINDOW_POS'] = f"{(monitorWidth - sizex) // 2}, {(monitorHeight - sizey) // 2}"
+    if fullscreen:
+        screen = pygame.display.set_mode([sizex, sizey], pygame.FULLSCREEN)
+    else:
+        screen = pygame.display.set_mode([sizex, sizey])
+    background = Background()
+    screen.fill(background.colour)
+    pygame.display.set_caption("Graphics Window")
+    background.surface = screen.copy()
+    pygame.display.update()
+    return screen
+
+def endWait():
+    updateDisplay()
+    print("Press ESC to quit")
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                waiting = False
+    pygame.quit()
+    exit()
+
+
+screenSize(800, 336)
+testSprite = makeSprite("animatedBG.png", 8)  # links.gif contains 32 separate frames of animation.
+
+moveSprite(testSprite, 400, 168, True)
+showSprite(testSprite)
+
+nextFrame = clock()
+frame = 0
+while True:
+    if clock() > nextFrame:                         # We only animate our character every 80ms.
+        frame = (frame+1) % 8                         # There are 8 frames of animation in each direction
+        nextFrame += 80                             # so the modulus 8 allows it to loop
+        changeSpriteImage(testSprite, 0*8+frame)    # 0*8 because right animations are the 0th set in the sprite sheet
+
+
+endWait()
